d3e8eb7262da1e7f5e4ff1ab72aba1ce26435b89
yyc <2243702705@qq.com>
2021-03-04 16:46:56 +0800

    created!

---
+++ src/views/SignUp.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="signup">
+    <div class="signup-wrapper">
+      <sign-up-core />
+    </div>
+  </div>
+</template>
+<script>
+import SignUpCore from "@/components/SignUpCore";
+
+export default {
+  components: {
+    SignUpCore
+  },
+  data() {
+    return {};
+  }
+};
+</script>
